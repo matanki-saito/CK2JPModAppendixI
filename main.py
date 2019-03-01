15d45945647c966e38ca6f58cc9fd94bac1d8c6c
matanki-saito <matanki.saito@gmail.com>
2019-03-02 05:19:13 +0900

    distファイルを作成するようにした

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import hashlib
 import json
 import os
 import shutil
+import tempfile
 import urllib.request
 import zipfile
-from datetime import datetime as dt
 from os.path import join
 
 from boto3.session import Session
@@ -16,14 +17,14 @@ _ = join
 
 def download_trans_zip_from_paratranz(project_id,
                                       secret,
-                                      base_url="https://paratranz.cn",
-                                      save_file_path="./tmp/paratranz.zip"):
+                                      out_file_path,
+                                      base_url="https://paratranz.cn"):
     """
     paratranzからzipをダウンロードする
     :param project_id:
     :param secret:
     :param base_url:
-    :param save_file_path:
+    :param out_file_path:
     :return:
     """
 
@@ -31,22 +32,24 @@ def download_trans_zip_from_paratranz(project_id,
     req = urllib.request.Request(request_url)
     req.add_header("Authorization", secret)
 
-    with open(save_file_path, "wb") as my_file:
+    with open(out_file_path, "wb") as my_file:
         my_file.write(urllib.request.urlopen(req).read())
+
+    return out_file_path
 
 
 def download_asset_from_github(repository_author,
                                repository_name,
+                               out_file_path,
                                release_tag=None,
-                               file_name=None,
-                               save_file_path="./tmp/font.zip"):
+                               file_name=None):
     """
     githubからアセットをダウンロード。未指定の場合は最新を取得
     :param repository_author:
     :param repository_name:
     :param release_tag:
     :param file_name:
-    :param save_file_path:
+    :param out_file_path:
     :return:
     """
     api_base_url = "https://api.github.com/repos/{}/{}".format(repository_author, repository_name)
@@ -69,75 +72,137 @@ def download_asset_from_github(repository_author,
                                                             )
     req = urllib.request.Request(request_url)
 
-    with open(save_file_path, "wb") as my_file:
+    with open(out_file_path, "wb") as my_file:
         my_file.write(urllib.request.urlopen(req).read())
 
+    return out_file_path
 
-def assembly_core_mod(font_asset_zip_path="./tmp/font.zip",
-                      paratranz_trans_zip_path="./tmp/paratranz.zip",
-                      core_folder_path="./out/CORE"):
+
+# TODO: フォント側を調整する必要がある。
+def filter_f(item):
+    if item.startswith("aoyagireisyo60-aoyagi") or \
+            item.startswith("aoyagireisyo60-appb") or \
+            item.startswith("tuikafont1"):
+        return False
+    else:
+        return True
+
+
+def assembly_core_mod_zip_file(resource_font_asset_zip_file_path,
+                               resource_image_file_path,
+                               resource_interface_dir_path,
+                               resource_paratranz_trans_zip_file_path,
+                               out_file_path):
     """
     コアモッドを作成
-    :param font_asset_zip_path:
-    :param paratranz_trans_zip_path:
-    :param core_folder_path:
+    :param resource_font_asset_zip_file_path:　githubにあるフォントアセットのzipファイルのパス
+    :param resource_paratranz_trans_zip_file_path: Paratranzからダウンロードできるzipファイルのパス
+    :param resource_image_file_path: 画像ファイルパス
+    :param resource_interface_dir_path: インターフェースディレクトリパス
+    :param out_file_path: 出力ファイルパス
     :return:
     """
 
-    # 出力フォルダをクリア
-    shutil.rmtree(core_folder_path, ignore_errors=True)
+    with tempfile.TemporaryDirectory() as temp_dir_path:
+        # 画像ファイル
+        shutil.copy(resource_image_file_path, temp_dir_path)
 
-    # shutilでの移動は、移動先ディレクトリがなくても自動で作成されるが、
-    # "正しく"動作しないので、予め作成しておく必要がある
-    # 具体的に言うと、構造が正しく保持されずに移動される
-    # 出力フォルダを作成する
-    os.makedirs(core_folder_path, exist_ok=True)
+        # interface
+        shutil.copytree(resource_interface_dir_path, _(temp_dir_path, "interface"))
 
-    # 画像ファイル
-    shutil.copy(_(".", "resource", "title.jpg"), core_folder_path)
+        # gfx
+        salvage_files_from_github_font_zip(out_dir_path=_(temp_dir_path, "gfx", "fonts"),
+                                           filter_f_f=filter_f,
+                                           resource_path=resource_font_asset_zip_file_path)
 
-    # interface
-    shutil.copytree(_(".", "resource", "interface"), _(core_folder_path, "interface"))
+        # localisation
+        salvage_files_from_paratranz_trans_zip(out_dir_path=_(temp_dir_path, "localisation"),
+                                               folder_list=["localisation"],
+                                               paratranz_zip_path=resource_paratranz_trans_zip_file_path)
 
-    # gfx
-    # フォント側を調整する必要がある。
-    def filter_f(item):
-        if item.startswith("aoyagireisyo60-aoyagi") or \
-                item.startswith("aoyagireisyo60-appb") or \
-                item.startswith("tuikafont1"):
-            return False
-        else:
-            return True
-
-    salvage_files_from_github_font_zip(out_dir_path=_(core_folder_path, "gfx", "fonts"),
-                                       filter_f=filter_f,
-                                       resource_path=font_asset_zip_path)
-
-    # localisation
-    salvage_files_from_paratranz_trans_zip(copy_dir_path=_(core_folder_path, "localisation"),
-                                           folder_list=["localisation"],
-                                           paratranz_zip_path=paratranz_trans_zip_path)
-
-    # zip化する
-    return shutil.make_archive(core_folder_path, 'zip', root_dir=core_folder_path)
+        # zip化する
+        return shutil.make_archive(out_file_path, 'zip', root_dir=temp_dir_path)
 
 
-def salvage_files_from_github_font_zip(out_dir_path, filter_f, resource_path="./tmp/font.zip"):
+def salvage_files_from_github_font_zip(out_dir_path,
+                                       filter_f_f,
+                                       resource_path):
     with zipfile.ZipFile(resource_path) as font_zip:
-        special_files = filter(filter_f, font_zip.namelist())
+        special_files = filter(filter_f_f, font_zip.namelist())
 
         font_zip.extractall(path=out_dir_path, members=special_files)
 
 
-def salvage_files_from_paratranz_trans_zip(copy_dir_path, paratranz_zip_path="./tmp/paratranz.zip",
+def salvage_files_from_paratranz_trans_zip(out_dir_path,
+                                           paratranz_zip_path,
                                            folder_list=[]):
     with zipfile.ZipFile(paratranz_zip_path) as paratranz_zip:
         special_files = filter(lambda name: name.startswith("special/"), paratranz_zip.namelist())
 
-        paratranz_zip.extractall(path=_(".", "tmp", "trans"), members=special_files)
+        with tempfile.TemporaryDirectory() as temp_dir_path:
+            paratranz_zip.extractall(path=temp_dir_path, members=special_files)
 
-    for folder in folder_list:
-        shutil.copytree(_(".", "tmp", "trans", "special", folder), copy_dir_path)
+            for folder in folder_list:
+                shutil.copytree(_(temp_dir_path, "special", folder), out_dir_path)
+
+
+def generate_dot_mod_file(mod_title_name,
+                          mod_file_name,
+                          mod_tags,
+                          mod_image_file_path,
+                          out_dir_path,
+                          mod_user_dir_name=None):
+    """
+    .mod.modファイルを作る
+    :param mod_title_name:
+    :param mod_file_name: zipファイルの名前（.zipを含まない）
+    :param mod_user_dir_name:ユーザ作業ディレクトリ名
+    :param mod_tags: Set<String>型
+    :param mod_image_file_path:
+    :param out_dir_path: 出力ディレクトリのパス
+    :return: 出力ファイルパス
+    """
+
+    os.makedirs(out_dir_path, exist_ok=True)
+
+    out_file_path = _(out_dir_path, "{}.mod.mod".format(mod_file_name))
+
+    if mod_user_dir_name is None:
+        mod_user_dir_name = mod_file_name
+
+    with open(out_file_path, "w", encoding="utf-8") as fw:
+        lines = [
+            'name="{}"'.format(mod_title_name),
+            'archive="mod/{}.zip"'.format(mod_file_name),
+            'user_dir="{}"'.format(mod_user_dir_name),
+            'tags={}'.format("{" + " ".join(mod_tags) + "}"),
+            'picture="{}"'.format(mod_image_file_path)
+        ]
+
+        fw.write("\n".join(lines))
+
+    return out_file_path
+
+
+def generate_distribution_file(url,
+                               mod_file_path,
+                               out_file_path):
+    """
+    trielaで使用する配布用設定ファイルを作成する。
+    :param url:
+    :param mod_file_path:
+    :param out_file_path:
+    :return:
+    """
+
+    with open(mod_file_path, 'rb') as fr:
+        md5 = hashlib.md5(fr.read()).hexdigest()
+
+    d_new = {'file_md5': md5,
+             'url': url}
+
+    with open(out_file_path, "w", encoding="utf-8") as fw:
+        json.dump(d_new, fw, indent=2, ensure_ascii=False)
 
 
 def upload_mod_to_s3(upload_file_path,
@@ -146,12 +211,47 @@ def upload_mod_to_s3(upload_file_path,
                      access_key,
                      secret_access_key,
                      region):
+    """
+    S3にファイルをアップロードする
+    :param upload_file_path:
+    :param name:
+    :param bucket_name:
+    :param access_key:
+    :param secret_access_key:
+    :param region:
+    :return: CDNのURL
+    """
     session = Session(aws_access_key_id=access_key,
                       aws_secret_access_key=secret_access_key,
                       region_name=region)
 
     s3 = session.resource('s3')
     s3.Bucket(bucket_name).upload_file(upload_file_path, name)
+
+    return "{}/{}".format("https://d3fxmsw7mhzbqi.cloudfront.net", name)
+
+
+def pack_mod(out_file_path,
+             mod_zip_path,
+             mod_title_name,
+             mod_file_name,
+             mod_tags,
+             mod_image_file_path,
+             mod_user_dir_name=None):
+    with tempfile.TemporaryDirectory() as temp_dir_path:
+        # .mod.modファイルを作成する
+        generate_dot_mod_file(
+            mod_title_name=mod_title_name,
+            mod_file_name=mod_file_name,
+            mod_tags=mod_tags,
+            mod_user_dir_name=mod_user_dir_name,
+            mod_image_file_path=mod_image_file_path,
+            out_dir_path=temp_dir_path)
+
+        # zipをコピー
+        shutil.copy(mod_zip_path, _(temp_dir_path, "{}.zip".format(mod_file_name)))
+
+        return shutil.make_archive(out_file_path, 'zip', root_dir=temp_dir_path)
 
 
 def main():
@@ -160,21 +260,58 @@ def main():
     os.makedirs(_(".", "out"), exist_ok=True)
 
     # フォントセットの最新版をダウンロードする
-    download_asset_from_github(repository_author="matanki-saito", repository_name="CK2Fontcreate")
+    font_file_path = download_asset_from_github(repository_author="matanki-saito",
+                                                repository_name="CK2Fontcreate",
+                                                out_file_path=_(".", "tmp", "font.zip"))
+
+    print("font_file_path:{}".format(font_file_path))
 
     # 翻訳の最新版をダウンロードする
-    download_trans_zip_from_paratranz(project_id=91, secret=os.environ.get("PARATRANZ_SECRET"))
+    p_file_path = download_trans_zip_from_paratranz(
+        project_id=91,
+        secret=os.environ.get("PARATRANZ_SECRET"),
+        out_file_path=_(".", "tmp", "paratranz.zip"))
+
+    print("p_file_path:{}".format(p_file_path))
 
     # コアModを構築する
-    core_mod_zip_path = assembly_core_mod()
+    core_mod_zip_file_path = assembly_core_mod_zip_file(
+        resource_font_asset_zip_file_path=font_file_path,
+        resource_paratranz_trans_zip_file_path=p_file_path,
+        resource_image_file_path=_(".", "resource", "title.jpg"),
+        resource_interface_dir_path=_(".", "resource", "interface"),
+        out_file_path=_(".", "tmp", "mod"))
 
-    # S3にアップロード
-    upload_mod_to_s3(upload_file_path=core_mod_zip_path,
-                     name=dt.now().strftime('%Y-%m-%d_%H-%M-%S-{}'.format("ck2-core")),
-                     bucket_name="triela-file",
-                     access_key=os.environ.get("AWS_S3_ACCESS_KEY"),
-                     secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
-                     region="ap-northeast-1")
+    print("core_mod_zip_file_path:{}".format(core_mod_zip_file_path))
+
+    # packする
+    mod_pack_file_path = pack_mod(
+        out_file_path=_(".", "out", "ck2_core_mod"),
+        mod_file_name="jpmod3_0_x",
+        mod_zip_path=core_mod_zip_file_path,
+        mod_title_name="Japanese Language Mod 3.0.x",
+        mod_tags={"Translation", "Localisation"},
+        mod_image_file_path="title.jpg",
+        mod_user_dir_name="JLM30X")
+
+    print("mod_pack_file_path:{}".format(mod_pack_file_path))
+
+    # S3にアップロード from datetime import datetime as dt
+    from datetime import datetime as dt
+    cdn_url = upload_mod_to_s3(
+        upload_file_path=mod_pack_file_path,
+        name=dt.now().strftime('%Y-%m-%d_%H-%M-%S-{}'.format("ck2-core")),
+        bucket_name="triela-file",
+        access_key=os.environ.get("AWS_S3_ACCESS_KEY"),
+        secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
+        region="ap-northeast-1")
+
+    print("cdn_url:{}".format(cdn_url))
+
+    # distributionファイルを生成する
+    generate_distribution_file(url=cdn_url,
+                               out_file_path=_(".", "out", "dist.v1.json"),
+                               mod_file_path=mod_pack_file_path)
 
 
 if __name__ == "__main__":
